4e0a96dde4663c37b97d2ab08a2232ff5562b88f
YoduYodu <wang7ong@gmail.com>
2019-11-21 10:56:57 -0800

    Exempt csrf

--- predictor/apps.py
+++ predictor/apps.py
@@ -2,11 +2,13 @@ from django.apps import AppConfig
 from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
 from fastai.text import untar_data, load_data, URLs, AWD_LSTM, text_classifier_learner
 import uuid, datetime, json
+from django.views.decorators.csrf import csrf_exempt
 
 
 class PredictorConfig(AppConfig):
     name = 'predictor'
     verbose_name = 'Sentiment predictor'
+
 
 bs = 12
 path = untar_data(URLs.IMDB)
@@ -16,32 +18,42 @@ learn.load_encoder('fine_tuned_enc')
 learn.load('fourth')
 
 
+@csrf_exempt
 def predict(req):
     # Validate request
-    if req.method != 'GET':
-        return HttpResponseNotAllowed(['GET'])
-    elif len(req.GET) != 2 or 'user_id' not in req.GET or 'text' not in req.GET:
-        return HttpResponseBadRequest('Bad Request')
+    if req.method == 'OPTIONS':
+        res = HttpResponse()
+        res['Allow'] = "['POST', 'OPTIONS']"
+        return res
+
+    elif req.method != 'POST':
+        return HttpResponseNotAllowed(['POST', 'OPTIONS'])
+
+    # Parse request body
+    props = json.loads(req.body.decode('utf-8'))
+    if len(props) != 2 or 'user_id' not in props or 'text' not in props:
+        return HttpResponseBadRequest('Bad Request', content_type='text/plain')
 
     # Parse request
-    props = req.GET
     user_id = props.get('user_id')
     text = props.get('text')
 
-    # Generate Prediction
+    # Generate Predictioncsrf
     res_json = dict()
     res_json['uuid'] = str(uuid.uuid4())
     res_json['user_id'] = user_id
     res_json['text'] = text
-    res_json['is_positive'] = True if learn.predict(text)[0] is 'pos' else False
-
+    res_json['is_positive'] = True if str(learn.predict(text)[0]) == 'pos' else False
     # Time
     time = datetime.datetime.now()
     res_json['time_date'] = "{}-{}-{}T{}:{}:{}Z"\
         .format(time.year, time.month, time.day, time.hour, time.minute, time.second)
     res_json['object'] = 'prediction'
 
-    return HttpResponse(json.dumps(res_json), content_type='application/json')
+    res = HttpResponse(json.dumps(res_json), content_type='application/json')
+    res["Access-Control-Allow-Origin"] = "*"
+    res['Access-Control-Allow-Methods'] = "'POST', 'OPTIONS']"
+    return res
 
 
 
