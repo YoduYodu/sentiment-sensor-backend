0d875c6122b389438f154b42408dbeff44c27197
YoduYodu <wang7ong@gmail.com>
2019-11-22 19:42:50 -0800

    Implement DB insertion and update

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
+## Sentiment Sensor
+
+### Core features
+* This project will provide an API, which will take review texts from users and predict whether it's a positive review or negative, and send the result back to users.
+* The server will also provide another API which will return 5 most recent reviews and their corresponding results, and send the response back to users.
+### Optional features
+* Another two APIs will send responses with total number pieces of texts that users has sent, and correctness percentage of total predictions made by the project. 
+* The users will be asked whether an output is correct, the feedback will be sent back to server, which will be another API handling user feedbacks.
+* Analysis on all requests that users made, e.g. how accurate this sensor made according to users' feedbacks, how many reviews are there in total. These will be handle solely in frontend, meaning after getting information from APIs, the frontend will visualization the info by using D3.js
+
+### Language and Tools
+* For backend, I will use Django to handle API and database.
+* Specifically, GraphQL will need to be used for API implementation, and PyMongo for MongoDB handling.
+* For frontend, React.js is needed for sending requests to server and D3.js for creating views.
+* Frontend will be deployed on GitHub Pages server, and backend server will be deployed on AWS.
+
+### Timeline
+
+* Week 5(Sep-22): Set up the environment for NLP modeling, including cloud GPU setup, Jupyter env setup, etc.
+* Week 6(Sep-27): Data collection / Parallel downloading / Creating a validation set, and Data cleaning, using the model to help us find data problems.
+* Week 7-8(Oct-11): Handle multi-label datasets by data blocks API
+* Week 9(__Milestone #1__)  Train the NLP model using my own dataset and understanding of NLP.
+* Week 10(Oct-25): To learn embedding layer, and try to optimize the model with it.
+* Week 11-12(Nov-8): To learn Dropout, Data augmentation, Batch normalization and convolutions, as well as data ethics
+* Week 13(Nov-15): Implementing production code for backend, and deploying.
+* Week 14(Nov-22): Implementing frontend and deploying.
+* Week 15(__Milestone #2__): Finish side project, cutting loose ends.
+
 ## Log
 ### CLI
 ```shell script
@@ -65,14 +93,28 @@ Response:
 }
 ```
 
+##### /users
+* GET /users?user_id=foo
+
+Response:
+```json
+{
+  "user_id": "foo",
+  "prediction_ids": [],
+  "object": "user"
+}
+```
+
 #### Nov 16
 * Modify Prediction model
 * Add Accuracy model
 
 #### Nov 18
 * Finish predictions api
+
+#### Nov 21
+* Add users model
 * Connect to MongoDb
 
 #### Next:
-* Implement models using MongoDB engine.
 * Change fastai models path
--- predictor/apps.py
+++ predictor/apps.py
@@ -1,8 +1,12 @@
+import datetime
+import json
+import uuid
+
+from pymongo import MongoClient
 from django.apps import AppConfig
-from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
-from fastai.text import untar_data, load_data, URLs, AWD_LSTM, text_classifier_learner
-import uuid, datetime, json
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
+from fastai.text import untar_data, load_data, URLs, AWD_LSTM, text_classifier_learner
 
 
 class PredictorConfig(AppConfig):
@@ -17,9 +21,15 @@ learn = text_classifier_learner(data_clas, AWD_LSTM, drop_mult=0.5)
 learn.load_encoder('fine_tuned_enc')
 learn.load('fourth')
 
+password = 'rAL7TKvq6G6HD3kG'
+client = MongoClient("mongodb+srv://wenti:{}@cluster0-rj4l5.gcp.mongodb.net/test?retryWrites=true&w=majority&ssl=true"
+                     "&ssl_cert_reqs=CERT_NONE".format(password))
+db = client.test
+predictions, users = db.predictions, db.users
+
 
 @csrf_exempt
-def predict(req):
+def predict(req: HttpRequest):
     # Validate request
     if req.method == 'OPTIONS':
         res = HttpResponse()
@@ -31,24 +41,38 @@ def predict(req):
 
     # Parse request body
     props = json.loads(req.body.decode('utf-8'))
-    if len(props) != 2 or 'user_id' not in props or 'text' not in props:
+    if len(props) != 1 or 'text' not in props:
         return HttpResponseBadRequest('Bad Request', content_type='text/plain')
 
     # Parse request
-    user_id = props.get('user_id')
+    user_id = req.COOKIES.get('user_id');
     text = props.get('text')
 
-    # Generate Predictioncsrf
+    # Generate Prediction
     res_json = dict()
-    res_json['uuid'] = str(uuid.uuid4())
+    res_json['prediction_id'] = str(uuid.uuid4())
     res_json['user_id'] = user_id
     res_json['text'] = text
     res_json['is_positive'] = True if str(learn.predict(text)[0]) == 'pos' else False
     # Time
     time = datetime.datetime.now()
-    res_json['time_date'] = "{}-{}-{}T{}:{}:{}Z"\
+    res_json['time_date'] = "{}-{}-{}T{}:{}:{}Z" \
         .format(time.year, time.month, time.day, time.hour, time.minute, time.second)
     res_json['object'] = 'prediction'
+
+    # Create prediction document in DB
+    predictions.insert_one({
+        "prediction_id": res_json['prediction_id'],
+        "user_id": user_id,
+        "text": text,
+        "is_positive": res_json['is_positive'],
+        "time_date": time,
+        "object": "prediction"
+    })
+
+    if user_id:
+        # Add prediction to users
+        add_prediction_to_user(user_id, res_json['prediction_id'])
 
     res = HttpResponse(json.dumps(res_json), content_type='application/json')
     res["Access-Control-Allow-Origin"] = "*"
@@ -56,5 +80,8 @@ def predict(req):
     return res
 
 
-
-
+def add_prediction_to_user(user_id: str, prediction_id: str):
+    users.insert_one(
+        {'user_id': user_id},
+        {'$push': {'prediction_ids': prediction_id}}
+    )
